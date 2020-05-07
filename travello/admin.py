f1560b7e516fa79804c753ed17f37bb00a1af86a
Amrutha-BS <51366149+Amrutha-BS@users.noreply.github.com>
2020-05-07 21:43:32 +0530

    Add files via upload

---
+++ travello/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Destination1,Destination2,Destination3,Destination4,Destination5
+
+# Register your models here.
+admin.site.register(Destination1)
+admin.site.register(Destination2)
+admin.site.register(Destination3)
+admin.site.register(Destination4)
+admin.site.register(Destination5)
